7cdc7d7908a8b415cee4ed0f193c5f7dcce3828c
ido barnea <barbareshet@gmail.com>
2016-11-13 09:24:20 +0200

    translation

--- inc/yna-wp-accessibility-control.php
+++ inc/yna-wp-accessibility-control.php
@@ -12,16 +12,16 @@ if (!defined('ABSPATH')){
 
     <a href="#accessibility-controls" tabindex="1" class="access-control-trigger">
 <!--        --><?php //_e('Accessibility', 'yna') ?>
-    <i class="fa fa-universal-access fa-3x"></i>
+    <i class="fa fa-universal-access fa-2x"></i>
     </a>
 
     <div id="accessibility-controls" class="accessibility-controls" tabindex="-1" role="dialog" aria-labelledby="accessibility-dialog-label">
         <h2 id="accessibility-dialog-label">
-            <?php _e('Site accessibility', 'yna') ?>
+            <?php _e('Site accessibility', 'ynaap_domain') ?>
         </h2>
         <div class="font-controls controls-group">
             <h3>
-                <?php _e('Change font size', 'yna'); ?>
+                <?php _e('Change font size', 'ynaap_domain'); ?>
             </h3>
             <div role="button"
                  tabindex="1"
@@ -29,8 +29,8 @@ if (!defined('ABSPATH')){
                  class="access-control change-font-size change-font-100"
                  data-access-type="font-size"
                  data-access-value="null"
-                 title="<?php _e('Change the font size to 100%', 'yna') ?>">
-                <?php _e('A', 'yna') ?>
+                 title="<?php _e('Change the font size to 100%', 'ynaap_domain') ?>">
+                <?php _e('A', 'ynaap_domain') ?>
             </div>
             <div role="button"
                  tabindex="1"
@@ -38,8 +38,8 @@ if (!defined('ABSPATH')){
                  class="access-control change-font-size change-font-150"
                  data-access-type="font-size"
                  data-access-value="150"
-                 title="<?php _e('Change the font size to 100%', 'yna') ?>">
-                <?php _e('A', 'yna') ?>
+                 title="<?php _e('Change the font size to 100%', 'ynaap_domain') ?>">
+                <?php _e('A', 'ynaap_domain') ?>
             </div>
             <div role="button"
                  tabindex="1"
@@ -47,37 +47,37 @@ if (!defined('ABSPATH')){
                  class="access-control change-font-size change-font-200"
                  data-access-type="font-size"
                  data-access-value="200"
-                 title="<?php _e('Change the font size to 200%', 'yna') ?>">
-                <?php _e('A', 'yna') ?>
+                 title="<?php _e('Change the font size to 200%', 'ynaap_domain') ?>">
+                <?php _e('A', 'ynaap_domain') ?>
             </div>
         </div>
         <div class="contrast-controls controls-group">
-            <h3><?php _e('Change contrast', 'yna') ?></h3>
+            <h3><?php _e('Change contrast', 'ynaap_domain') ?></h3>
             <div role="button" tabindex="1" aria-checked="true" class="access-control change-contrast change-contrast-normal" data-access-type="contrast" data-access-value="null"
-                 title="<?php _e('Normal contrast', 'yna') ?>">
-                <?php _e('Normal contrast', 'yna') ?>
+                 title="<?php _e('Normal contrast', 'ynaap_domain') ?>">
+                <?php _e('Normal contrast', 'ynaap_domain') ?>
             </div>
             <div role="button" tabindex="1" aria-checked="false" class="access-control change-contrast change-contrast-gray" data-access-type="contrast" data-access-value="gray"
-                 title="<?php _e('Grayscale', 'yna') ?>">
-                <?php _e('Grayscale', 'yna') ?>
+                 title="<?php _e('Grayscale', 'ynaap_domain') ?>">
+                <?php _e('Grayscale', 'ynaap_domain') ?>
             </div>
             <div role="button" tabindex="1" aria-checked="false" class="access-control change-contrast change-contrast-high" data-access-type="contrast" data-access-value="high"
-                 title="<?php _e('High contrast', 'yna') ?>">
-                <?php _e('High contrast', 'yna') ?>
+                 title="<?php _e('High contrast', 'ynaap_domain') ?>">
+                <?php _e('High contrast', 'ynaap_domain') ?>
             </div>
         </div>
         <div class="flash-controls controls-group">
-            <h3><?php _e('Block flashes', 'yna') ?></h3>
+            <h3><?php _e('Block flashes', 'ynaap_domain') ?></h3>
             <div role="button" tabindex="1" aria-checked="false" class="access-control flash-block on" data-access-type="transition" data-access-value="on"
-                 title="<?php _e('On', 'yna') ?>">
-                <?php _e('On', 'yna') ?>
+                 title="<?php _e('On', 'ynaap_domain') ?>">
+                <?php _e('On', 'ynaap_domain') ?>
             </div>
             <div role="button" tabindex="1" aria-checked="true" class="access-control flash-block off" data-access-type="transition" data-access-value="off"
-                 title="<?php _e('Off', 'yna') ?>">
-                <?php _e('Off', 'yna') ?>
+                 title="<?php _e('Off', 'ynaap_domain') ?>">
+                <?php _e('Off', 'ynaap_domain') ?>
             </div>
         </div>
-        <a href="#accessibility-controls" tabindex="1" class="access-control-close"><?php _e('Close', 'yna') ?></a>
+        <a href="#accessibility-controls" tabindex="1" class="access-control-close"><?php _e('Close', 'ynaap_domain') ?></a>
     </div>
 
 
--- yna-wp-accessibility-plugin.php
+++ yna-wp-accessibility-plugin.php
@@ -7,7 +7,7 @@
  * Author: Y&A
  * Author URI: http:www.yna.co.il
  * Plugin Site: http://www.yna.co.il/%D7%A0%D7%92%D7%99%D7%A9%D7%95%D7%AA-%D7%90%D7%AA%D7%A8%D7%99-%D7%90%D7%99%D7%A0%D7%98%D7%A8%D7%A0%D7%98/
- * 
+ * Textdomain: ynaap_domain
  **/
 
 //Exit if accessed directly
@@ -29,3 +29,9 @@ require_once (plugin_dir_path(__FILE__) . 'inc/yna-wp-accessibility-control.php'
 if (is_admin()){
     require_once (plugin_dir_path(__FILE__) . 'inc/yna-wp-accessibility-settings.php');
 }
+
+//Load translation
+function ynaap_load_textdomain() {
+    load_plugin_textdomain( 'yna-wp-accessibility', false, dirname( plugin_basename(__FILE__) ) . '/lang/' );
+}
+add_action('plugins_loaded', 'ynaap_load_textdomain');
